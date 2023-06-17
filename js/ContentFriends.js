66c71abed5fdceda59ad4ceb2673f82839ae7c29
Sayan Paul <sayanpaul741127@gmail.com>
2023-06-17 11:28:41 +0530

    Version: 0.0.4

--- js/ContentFriends.js
+++ js/ContentFriends.js
@@ -1,70 +1,87 @@
-// Extension will add a new box to sidebar
-let element = document.getElementById(`sidebar`);
+// Content addition to id="sidebar" of CF Problem-Page 
+let sidebarID = document.getElementById(`sidebar`);
 
-element.innerHTML += `
+// Adding a new box-section to sidebar
+/*  The Box will contain heading and button as follows:
+*   → Accepted Codes of Friends     
+*     Show Codes
+*/
+sidebarID.innerHTML += `
 <div class="roundbox sidebox sidebar-menu borderTopRound " style="">
     <div class="caption titled">→ Accepted Codes of Friends</div>
     <div>
         <ul>
             <li>
                 <span>
-                    <span id="myBtn">Show Codes</span>
+                    <span id="showCodeButton">Show Codes</span>
                 </span>
             </li>
         </ul>
     </div>
 </div>`;
 
-// adding modal to the pageContent
 
+// Adding Modal to the pageContent to Show Codes on clicking
 let pageContentID = document.getElementById(`pageContent`);
-
-pageContentID.innerHTML += `<!-- The Modal -->
-<div id="myModal" class="modal">
-
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close">&times;</span>
-    <pre id="modalParapre" class="prettyprint lang-cpp linenums program-source"><code id="modalPara" class="cpp"><span class="ModalHeader">→ Accepted Codes of Friends</span><hr></code></pre>
+/*  The 3 layers of Modals as follows:
+*   Modal (default hidden)
+*     Modal Contents 
+*       Modal Codes (section for codes) & Modal Close (closing button)
+*         Modal Code Header
+*/
+pageContentID.innerHTML += `
+<div id="modal" class="modal">
+  <div class="modalContent">
+    <span class="modalClose">&times;</span>
+    <pre id="modalCode" ><code><span class="modalCodeHeader">→ Accepted Codes of Friends</span><hr></code></pre>
   </div>
-
 </div>;`
 
-// Adding Highlighting Tools in head of content page
-document.head.insertAdjacentHTML(`beforeend`, `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/monokai-sublime.min.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/highlight.min.js">hljs.initHighlightingOnLoad();hljs.highlightAll();</script>`);
-
-
+/*
+*   Declaring variables corresponding to class and id
+*     Adding Modal effects on clicking 
+*/
 
 // Get the modal
-var modal = document.getElementById("myModal");
+const modalID = document.getElementById("modal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+const showCodeButtonID = document.getElementById("showCodeButton");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const modalCloseID = document.getElementsByClassName("modalClose")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function () {
-  modal.style.display = "block";
+// Get the Modal Code in which codes wil be added 
+const modalCodeID = document.getElementById(`modalCode`);
+
+// When the user clicks the Show Code button, open the modal 
+showCodeButtonID.onclick = function () {
+  modalID.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
+// When the user clicks on <span> (x), i.e Modal Close, close the modal
+modalCloseID.onclick = function () {
+  modalID.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modal, close modal
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modalID) {
+    modalID.style.display = "none";
   }
 }
 
-// adding the logic to fetch Codeforces API and fetch codes and change the innerHTML to the modalPara
 
-let modalParaID = document.getElementById(`modalPara`);
+/*
+*   Declaring and defining functions 
+*     1. getProblemIdFromUrl()     => Extract problem ID from the current URL
+*     2. getContestIdFromUrl()     => Extract contest ID from the current URL
+*     3. getFriendsUsernameList()  => Extract Handles of User's CF Friends
+*     4. getSubmissionId()         => Extacting Submission ID corresponding to contest ID, submission ID and Handle
+*     5. fetchCodeFromSubmission() => Extract codes from contest ID and submission ID
+*     6. sleep()                   => Sleep Function to reduce URL fetch-rate
+*     7. Add Fetched Codes to Modal
+*/
 
 // Extract problem ID from the current URL like A or B or C
 function getProblemIdFromUrl() {
@@ -74,8 +91,7 @@ function getProblemIdFromUrl() {
   return problemId;
 }
 
-
-// Extract contest ID from the current URL like 1830, 1836 $
+// Extract contest ID from the current URL like 1830, 1836 
 function getContestIdFromUrl() {
   const url = new URL(window.location.href);
   const pathSegments = url.pathname.split('/');
@@ -96,40 +112,7 @@ function getContestIdFromUrl() {
   return null;
 }
 
-
-
-// fetchcode from contestid and submission id
-
-// non-highlighted
-async function fetchCodeFromSubmission(contest_id, submission_id) {
-  return fetch(`https://codeforces.com/contest/${contest_id}/submission/${submission_id}`)
-    .then(function (res) {
-      return res.text();
-    })
-    .then(async function (textHtml) {
-      // console.log(textHtml)
-      // Regular expression pattern to extract the content between <pre id="program-source-text"> and </pre>
-      const regexPattern = /<pre[^>]*id="program-source-text"[^>]*>(.*?)<\/pre>/s;
-
-      // Extract the content using the regular expression
-      const match = textHtml.match(regexPattern);
-
-      // Check if a match is found and extract the code
-      if (match && match.length > 1) {
-        let code = match[1];
-        return code;
-      } else {
-        return "No result found !";
-      }
-    })
-    .catch(function (_) {
-      return "No submission found !";
-    });
-}
-
-
-
-// Fetch handles of Friends
+// Extract Handles of User's CF Friends 
 async function getFriendsUsernameList() {
   return fetch("https://codeforces.com/friends")
     .then(function (res) {
@@ -145,7 +128,7 @@ async function getFriendsUsernameList() {
 
       // parse usernames
 
-      // $ Select the parent element with class "datatable"
+      //  Select the parent element with class "datatable"
         const datatableElement = document.getElementsByClassName('datatable')[0];
 
         // Select the elements with class "rated-user" inside the "datatable" element /$
@@ -157,16 +140,11 @@ async function getFriendsUsernameList() {
     })
 }
 
+// Assigning Problem ID and Contest ID to respective variables 
+const Problem_ID_const = getProblemIdFromUrl(); // console.log(Problem_ID_const);
+const Contest_ID_const = getContestIdFromUrl(); // console.log(Contest_ID_const);
 
-// code of sayan 
-
-
-let prblid = getProblemIdFromUrl(); // console.log(prblid);
-let cntstid = getContestIdFromUrl(); // console.log(cntstid);
-
-
-
-// Extacting Submission ID corresponding to contest id, problem id and handle $
+// Extacting Submission ID corresponding to contest ID, submission ID and Handle
 async function getSubmissionId(contestIds, problemIds, handles) {
   let response = await fetch(`https://codeforces.com/api/contest.status?contestId=${contestIds}&from=1&count=10&handle=${handles}`);
   let data = await response.json();
@@ -183,78 +161,86 @@ async function getSubmissionId(contestIds, problemIds, handles) {
     return submissions[0].id;
   }
 
-  // Return null if no submission with 'OK' verdict found
+  // Return -1 if no submission with 'OK' verdict found
   return -1;
-
-}
-  
-
-
-
-// Sleep Function to hold a Program to execute
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+// Extract codes from contest ID and submission ID
+async function fetchCodeFromSubmission(contest_id, submission_id) {
+  return await fetch(`https://codeforces.com/contest/${contest_id}/submission/${submission_id}`)
+    .then(async function (res) {
+      return await res.text();
+    })
+    .then(async function (textHtml) {   // console.log(textHtml)
+      // Regular expression pattern to extract the content between <pre id="program-source-text"> and </pre>
+      const regexPattern =  /<pre[^>]*id="program-source-text"[^>]*>(.*?)<\/pre>/s;
 
+      // Extract the content using the regular expression
+      const match =  textHtml.match(regexPattern);
 
+      // Check if a match is found and extract the code
+      if (match && match.length > 1) {
+        let code = match[1];
+        return code;
+      } else {
+        return "No result found !";
+      }
+    })
+    .catch(function (_) {
+      return "No submission found !";
+    });
+}
 
+// Sleep Function to reduce URL fetch-rate
+function sleep(miliSecond) {
+  return new Promise(resolve => setTimeout(resolve, miliSecond));
+}
 
-let contestId = Number(cntstid);
-let problemId = String(prblid);
-let handle = 'sapaul';
+// Variables for reducing code execution
+let count =0;
+let start = new Date();
+
+// Variables to be passed in function getSubmissionId()
+let contestId = Number(Contest_ID_const);
+let problemId = String(Problem_ID_const);
 
 // Getting list of friends using parser in a promise object
 let friendsPromise = getFriendsUsernameList();
 
-// Variables for making the code to wait 
-let count =0;
-let start = new Date();
-
-// Prints fetched code in Modal
+// Add Fetched Codes to Modal
 friendsPromise.then(async function (data) {
+  // For each Handle Fetch and Add Codes to Modal
   for (let i = 0; i < data.length; i++) {
-    handle = data[i];
-    // console.log(handle);
+    let handle = data[i];            // console.log(handle);
 
     // Print Submission ID in console
-    let submissionId = await getSubmissionId(contestId, problemId, handle);
-    // console.log(submissionId);
-
-      // made the function to wait
-      count++;
-      // console.log(count);
-      let current = new Date();
-      if ((count % 6) == 0) {
-        await sleep(1000);
-        if ((current - start) < 5000) {
-          await sleep(3500);
-        }
-        start = new Date();
-      }// end wait function
-
-
+    let submissionId = await getSubmissionId(contestId, problemId, handle);   // console.log(submissionId);
     
-    // Add code to Modal Corresponding to Submission ID
+    // made the function to wait to reduce fetch-rate to avoid being block
+    count++;                          // console.log(count);
+    let current = new Date();
+    if ((count % 6) == 0) {
+      await sleep(1000);
+      if ((current - start) < 5000) {
+        await sleep(3500);
+      }
+      start = new Date();
+    }// end wait function
+    
+    // when submission with verdict "ok" not found, don't add any code
+    if (submissionId == -1 ) {
+      continue;
+    }
+
+    // Add Codes to Modal Corresponding to Submission ID
     await fetchCodeFromSubmission(contestId, Number(submissionId))
     .then(async function (Code) {
-        modalParaID.innerHTML += `<span id="TextModal">Code submitted by </span><span id="HandleModal">${handle}\n\n </span>`;
+        modalCodeID.innerHTML += `<span id="TextModal">Code submitted by </span><span id="HandleModal">${handle}\n\n </span>`;
 
         // Add a span element with a CSS class around the Code variable
-        modalParaID.innerHTML += `<span class="codeSnippet">${Code}\n\n</span>`;
+        modalCodeID.innerHTML += `<span class="codeSnippet">${Code}\n\n</span>`;
 
       });
   }
 });
 
-
-
-// Manually highlight the code using highlight.js
-document.addEventListener('DOMContentLoaded', function() {
-  // Code that uses hljs goes here
-  const codeBlocks = document.querySelectorAll("pre code");
-  codeBlocks.forEach((block) => {
-    hljs.highlightBlock(block);
-  });
-  hljs.highlightAll();
-});
--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,19 @@
-function goProfile() {
+// Opens Handle in New Tab 
+function goDeveloperProfile() {
     const profileUrl = 'https://codeforces.com/profile/sapaul';
+    
+    //  Creating a New tab 
     chrome.tabs.create({ url: profileUrl });
 }
 
+// Opening Developer's Handle on Click
 document.addEventListener('DOMContentLoaded', function () {
-    const btn = document.getElementById('btn');
-    btn.addEventListener('click', goProfile);
+    const developerHandleID = document.getElementById('developerHandle');
+    const developerContactID = document.getElementById('developerContact');
+    
+    // Click on Developer Contact Section 
+    developerHandleID.addEventListener('click', goDeveloperProfile);
+    developerContactID.addEventListener('click', goDeveloperProfile);
 });
 
 
